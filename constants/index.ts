a519b17421054b0b5dbe9d1250662b86fb500ac8
wan.mureithi <estherdee27@gmail.com>
2025-01-02 18:07:12 +0300

    setup template

---
+++ constants/index.ts
@@ -0,0 +1,86 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/', key: 'how_hilink_work', label: 'How Hilink Work?' },
+  { href: '/', key: 'services', label: 'Services' },
+  { href: '/', key: 'pricing ', label: 'Pricing ' },
+  { href: '/', key: 'contact_us', label: 'Contact Us' },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  '/person-1.png',
+  '/person-2.png',
+  '/person-3.png',
+  '/person-4.png',
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: 'Tailored Travel Itineraries',
+    icon: '/map.svg',
+    variant: 'green',
+    description:
+      'We specialize in creating personalized travel plans, thoughtfully tailored to your preferences. Whether it’s a wine farm tour, a vibrant concert experience, or a serene yoga retreat, we ensure your journey reflects your unique interests and desires.',
+  },
+  {
+    title: 'Finding Comfortable Stays, Anywhere',
+    icon: '/calendar.svg',
+    variant: 'green',
+    description:
+      "Whether you’re seeking a luxurious hotel, a cozy boutique stay, or a budget-friendly option, we’ve got you covered. With trusted partners like Sarova and a wide range of choices, we ensure your accommodations match your preferences and needs.",
+  },
+  {
+    title: 'Hassle-Free Air Ticketing',
+    icon: '/tech.svg',
+    variant: 'green',
+    description:
+      'With trusted partnerships like Kenya Airways and Qatar Airways, we handle your flight bookings efficiently. From finding the best routes to securing competitive fares, we make sure you reach your destination with ease and comfort.',
+  },
+  {
+    title: 'Exclusive Concert & Event Travel',
+    icon: '/location.svg',
+    variant: 'orange',
+    description:
+      'Elevate your travel with our unique concert and event packages. From securing tickets to curating full itineraries around your favorite events, we create unforgettable experiences for music lovers and adventurers alike.',
+  },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: 'Learn More',
+    links: [
+      'About Hilink',
+      'Press Releases',
+      'Environment',
+      'Jobs',
+      'Privacy Policy',
+      'Contact Us',
+    ],
+  },
+  {
+    title: 'Our Community',
+    links: ['Climbing xixixi', 'Hiking hilink', 'Hilink kinthill'],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: 'Contact Us',
+  links: [
+    { label: 'Admin Officer', value: '123-456-7890' },
+    { label: 'Email Officer', value: 'hilink@akinthil.com' },
+  ],
+};
+
+export const SOCIALS = {
+  title: 'Social',
+  links: [
+    '/facebook.svg',
+    '/instagram.svg',
+    '/twitter.svg',
+    '/youtube.svg',
+    '/wordpress.svg',
+  ],
+};
